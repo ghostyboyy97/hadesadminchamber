570fb18c0e1faab7ee59a6be9580337a719dd9da
ghostyboyy97 <revghostmusic@gmail.com>
2025-06-14 12:13:50 -0400

    feat: react tinkering and set up base API calls

--- hadesadminchamber/src/app/page.tsx
+++ hadesadminchamber/src/app/page.tsx
@@ -1,103 +1,106 @@
+'use client';
+
 import Image from "next/image";
+import { useState, useCallback } from "react";
+
+
+
+// TODO
+// 1 - we need UI to prompt the user to go to https://cors-anywhere.herokuapp.com/corsdemo and request temporary access
+// 2 - we need UI to prompt the user for their API key
+// 3 - checkAuth()
+// 4 - check which games the user is a moderator of/check user ID against mod list
+// 5 - setup Get Next Run button to grab runs from relevant queues
+// 6 - (after subsequent step setup) - check hold/active verifying flags in database and check timeouts there; assign a valid run to current verifier
+// 7 - put run into the page
+// 8 - setup UI for changing each field on the run that's relevant
+// 9 - setup retiming tools built into page
+// 10 - auto-gen mod retime message if timestamp doesn't match (w/ ms handling for h2)
+// 11 - append message to stored desc
+// 12 - push verified/rejected state to sr.com with API key for verification
+// 13 - prompt user if they want to verify another run
+
+
+
+interface keyInputProps {
+  placeholder: string;
+  blur: () => {};
+}
+interface runButtonProps {
+  title: string;
+  click: () => {};
+}
+
+function GetRunButton({ title, click }: runButtonProps) {
+  return (
+    <button onClick={click} className="w-sm bg-sky-500 hover:bg-sky-700 cursor-pointer mt-8">{title}</button>
+  );
+}
+
+function APIKeyInput({ placeholder, blur }: keyInputProps) {
+  return (
+    <input onBlur={blur} className="w-lg h-8 p-2 mt-4 border border-white rounded-sm" type="text" placeholder={placeholder}></input>
+  )
+}
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+  let runList = [];
+  const HADES = "o1y9okr6";
+  const [value, setValue] = useState("Change me");
+  function handleChange(event: React.ChangeEvent<HTMLInputElement>) {
+    console.log(event.currentTarget.value);
+    setValue(event.currentTarget.value)
+  }
+  function checkKeyValid() {
+    console.log(value)
+  }
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+  async function getH1Runs() {
+    const url = `https://www.speedrun.com/api/v1/runs?game=${HADES}&orderby=submitted&direction=desc`;
+    try {
+      let response = await fetch(url, {
+        method: "GET",
+      });
+      if (!response.ok) {
+        throw new Error(`Response status: ${response.status}`)
+      }
+      const json = await response.json();
+      console.log(json)
+    } catch (exc: any) {
+      console.error(exc.message)
+    }
+  }
+
+
+  async function checkAuth() {
+    const url = "https://cors-anywhere.herokuapp.com/https://www.speedrun.com/api/v1/profile";
+    // const url = "https://www.speedrun.com/api/v1/profile";
+    try {
+      console.log(value)
+      let response = await fetch(url, {
+        method: "GET",
+        headers: {
+          "Host": "www.speedrun.com",
+          "Accept": "application/json",
+          "X-API-Key": value,
+        }
+      });
+      if (!response.ok) {
+        throw new Error(`Response status: ${response.status}`);
+      }
+      const json = await response.json();
+      console.log(json)
+    } catch (exc: any) {
+      console.error(exc.message)
+    }
+  }
+
+
+  return (
+    <div className="flex flex-col justify-start items-center w-dvw h-dvh">
+      <h1 className="font-bold text-8xl w-fit">Hades Admin Chamber</h1>
+      <APIKeyInput blur={checkAuth} placeholder="Enter your sr.com API key here" />
+      <GetRunButton click={getH1Runs} title="Get next run in queue" />
     </div>
   );
 }
